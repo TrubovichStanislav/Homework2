172c9bb89e93cde82daa3f0c4aa062e037ab658a
stas <nestas67@mail.ru>
2022-12-04 12:10:31 +0300

    HW2

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 4
+        var friend = 19;
+        friend += 2;
+        friend /= 7;
+        System.out.println(friend);
+        System.out.println();
+
+        //Задание 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *= 10;
+        System.out.println(frog);
+        frog /= 3.5;
+        System.out.println(frog);
+        frog += 4;
+        System.out.println(frog);
+    }
+}
